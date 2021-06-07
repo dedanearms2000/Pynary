7cf0fd42c770bfd148e8f047b404b0d27321fe7b
dedannyarms2000 <84487421+dedannyarms2000@users.noreply.github.com>
2021-06-07 17:20:02 -0400

    Add files via upload

---
+++ Binary.py
@@ -0,0 +1,22 @@
+import random
+def main():
+    running = 1
+    while running == 1:
+        binarne01 = random.randint(0, 1)
+        binarne02 = random.randint(0, 1)
+        binarne03 = random.randint(0, 1)
+        binarne04 = random.randint(0, 1)
+        binarne05 = random.randint(0, 1)
+        binarne06 = random.randint(0, 1)
+        binarne07 = random.randint(0, 1)
+        binarne08 = random.randint(0, 1)
+        binarne09 = random.randint(0, 1)
+        binarne10 = random.randint(0, 1)
+        binarne11 = random.randint(0, 1)
+        binarne12 = random.randint(0, 1)
+        binarne13 = random.randint(0, 1)
+        binarne14 = random.randint(0, 1)
+        binarne15 = random.randint(0, 1)
+        binarne16 = random.randint(0, 1)
+        print (binarne01, binarne02, binarne03, binarne04, binarne05, binarne06, binarne07, binarne08, binarne09, binarne10, binarne11, binarne12, binarne13, binarne14, binarne15, binarne16)
+main()
